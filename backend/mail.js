bd222d73455c16318bf58e35018fe5c398f9820d
Thomas Neos <tneos@hotmail.com>
2023-04-15 17:12:10 +0100

    Connect to database

--- backend/mail.js
+++
@@ -1,6 +0,0 @@
-const Mailjet = require("node-mailjet");
-
-const mailjet = new Mailjet({
-  apiKey: process.env.MJ_APIKEY_PUBLIC || "your-api-key",
-  apiSecret: process.env.MJ_APIKEY_PRIVATE || "your-api-secret",
-});
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const cors = require("cors");
 const corsOptions = {
   origin: "*",
@@ -10,17 +11,18 @@ const log = console.log;
 const app = express();
 app.use(cors(corsOptions));
 
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
+
+// Connect to Database
+connectDB();
 
 // Data parsing
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 // Post data endpoint
-app.post("/email", (req, res) => {
-  // TODO
-  // send email here
-  console.log(req.body);
+app.post("/message", (req, res) => {
+  log(req.body);
   res.json({message: "Message received!!"});
 });
 
