9c5f2965d2ecc77f1339e7ebcb3cef9923428d55
Thomas Neos <tneos@hotmail.com>
2023-05-01 18:47:02 +0100

    Use javascript for smooth scrolling

--- client/src/js/smoothScroll.js
+++ client/src/js/smoothScroll.js
@@ -1,37 +1,15 @@
 let projectsLink = document.getElementById("projects-link");
 let projectsSection = document.getElementById("projects");
 
-// projectsLink.addEventListener("click", e => {
-//   e.preventDefault();
-//   let scrollContainer = projectsSection;
-//   do {
-//     //find scroll container
-//     scrollContainer = scrollContainer.parentNode;
-//     if (!scrollContainer) return;
-//     scrollContainer.scrollTop += 1;
-//   } while (scrollContainer.scrollTop == 0);
+let aboutMeBtn = document.getElementById("contact-link");
+let contactSection = document.getElementById("contact");
 
-//   let targetY = 0;
-//   do {
-//     //find the top of target relatively to the container
-//     if (projectsSection == scrollContainer) break;
-//     targetY += projectsSection.offsetTop;
-//   } while ((projectsSection = projectsSection.offsetParent));
+const scrollDown = (link, section) => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    section.scrollIntoView();
+  });
+};
 
-//   scroll = function (c, a, b, i) {
-//     i++;
-//     if (i > 30) return;
-//     c.scrollTop = a + ((b - a) / 30) * i;
-//     setTimeout(function () {
-//       scroll(c, a, b, i);
-//     }, 20);
-//   };
-//   // start scrolling
-//   scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-//});
-
-// const smoothScroll = e => {
-//   e.preventDefault();
-
-//   projectsSection.scrollIntoView();
-// };
+scrollDown(projectsLink, projectsSection);
+scrollDown(aboutMeBtn, contactSection);
