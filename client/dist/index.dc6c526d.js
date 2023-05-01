b31ed68b44f7b155b16f17e05e3f10882616939c
Thomas Neos <tneos@hotmail.com>
2023-05-01 18:12:55 +0100

    Use href for smooth scrolling

--- client/dist/index.dc6c526d.js
+++ client/dist/index.dc6c526d.js
@@ -557,34 +557,37 @@ function hmrAccept(bundle, id) {
 }
 
 },{}],"ax0jI":[function(require,module,exports) {
-const projectsLink = document.getElementById("projects-link");
-let projectsSection = document.getElementById("projects");
-projectsLink.addEventListener("click", (e)=>{
-    e.preventDefault();
-    let scrollContainer = projectsSection;
-    do {
-        //find scroll container
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
-    }while (scrollContainer.scrollTop == 0);
-    let targetY = 0;
-    do {
-        //find the top of target relatively to the container
-        if (projectsSection == scrollContainer) break;
-        targetY += projectsSection.offsetTop;
-    }while (projectsSection = projectsSection.offsetParent);
-    scroll = function(c, a, b, i) {
-        i++;
-        if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function() {
-            scroll(c, a, b, i);
-        }, 20);
-    };
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-});
+let projectsLink = document.getElementById("projects-link");
+let projectsSection = document.getElementById("projects"); // projectsLink.addEventListener("click", e => {
+ //   e.preventDefault();
+ //   let scrollContainer = projectsSection;
+ //   do {
+ //     //find scroll container
+ //     scrollContainer = scrollContainer.parentNode;
+ //     if (!scrollContainer) return;
+ //     scrollContainer.scrollTop += 1;
+ //   } while (scrollContainer.scrollTop == 0);
+ //   let targetY = 0;
+ //   do {
+ //     //find the top of target relatively to the container
+ //     if (projectsSection == scrollContainer) break;
+ //     targetY += projectsSection.offsetTop;
+ //   } while ((projectsSection = projectsSection.offsetParent));
+ //   scroll = function (c, a, b, i) {
+ //     i++;
+ //     if (i > 30) return;
+ //     c.scrollTop = a + ((b - a) / 30) * i;
+ //     setTimeout(function () {
+ //       scroll(c, a, b, i);
+ //     }, 20);
+ //   };
+ //   // start scrolling
+ //   scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+ //});
+ // const smoothScroll = e => {
+ //   e.preventDefault();
+ //   projectsSection.scrollIntoView();
+ // };
 
 },{}]},["2q85e","ax0jI"], "ax0jI", "parcelRequire2041")
 
